f5967b92b71b206830eff11e143d67ab4e6ed7a8
nighttwo1 <ys12j1@gmail.com>
2024-02-19 18:50:07 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+[![Github All Releases](https://img.shields.io/github/v/release/nighttwo1/android-base-project)]()
+[![Github All Releases](https://img.shields.io/github/downloads/nighttwo1/android-base-project/total.svg)]()
+# Multi-module Base project for Android
+base project for android clean architecture, mvvm and multi-module
+
+- Modularize base on Android clean architecture layers
+- Http Client with Retrofit and kotlin serialization
+- Hilt
+
+### Module Dependencies
+![image](https://github.com/nighttwo1/android-base-project/assets/43779571/c5586877-950e-4273-8ea0-6d4b7dc79a7e)
+
+---
+## How to make new project with given base project
+### 1. Clone project
+```shell
+git clone git@github.com:nighttwo1/android-base-project.git
+```
+
+### 2. Save Project as template
+- File | New Projects Setup | Save Project As Template
+- Choose <whole project>
+- write down name and description for template
+
+### 3. Make new project
+- choose saved template
---
+++ data/src/main/kotlin/com/nighttwo1/data/module/ApiModule.kt
@@ -0,0 +1,50 @@
+package com.nighttwo1.data.module
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.nighttwo1.data.BuildConfig
+import com.nighttwo1.data.service.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+    @Provides
+    fun provideBaseUrl() = BuildConfig.API_BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        baseUrl: String,
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+}
